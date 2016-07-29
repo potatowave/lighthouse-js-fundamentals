23a2bda1710a638052390ffe9e2f3471d5b00569
Mike Jenkins <kinetic@icloud.com>
2016-07-29 20:19:55 +0000

    Added age calculator

---
+++ age-calculator.js
@@ -0,0 +1,8 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+  var age;
+  age = currentYear - yearOfBirth
+  return name + " is " + age + " years old."
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
